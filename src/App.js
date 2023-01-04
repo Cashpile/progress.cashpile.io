73ff33687e361ecd0f841436f2a9c913ad027e43
Joonbeom Kwon <djbkwon@gmail.com>
2023-01-04 11:41:07 -0500

    cache buster

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+import CacheBuster from 'react-cache-buster'
+import packageInfo from '../package.json'
+
 import Header from './components/Header'
+
 import Home from './pages/Home'
 import Week_0 from './pages/weeks/0'
 import Week_1 from './pages/weeks/1'
 import Week_2 from './pages/weeks/2'
 
 const App = () => {
+  const isProduction = process.env.NODE_ENV === 'production'
   return (
-    <BrowserRouter>
-      <Header />
-      <Route exact path='/' component={Home} />
-      <Route path='/0' component={Week_0} />
-      <Route path='/1' component={Week_1} />
-      <Route path='/2' component={Week_2} />
-    </BrowserRouter>
+    <CacheBuster
+      currentVersion={packageInfo.version}
+      isEnabled={isProduction}
+      isVerboseMode={true}
+      metaFileDirectory={'public'}
+    >
+      <BrowserRouter>
+        <Header />
+        <Route exact path='/' component={Home} />
+        <Route path='/0' component={Week_0} />
+        <Route path='/1' component={Week_1} />
+        <Route path='/2' component={Week_2} />
+      </BrowserRouter>
+    </CacheBuster>
   )
 }
 
